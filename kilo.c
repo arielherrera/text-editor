98b9b6a822e8d255ec95912234b75627339dbcdf
arielherrera <ariel.a.herrera@vanderbilt.edu>
2020-05-15 07:06:13 -0700

    added dyanmic string type and destructor but did not add comments

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -190,6 +191,33 @@ int getWindowSize(int *rows, int *cols) {
     *rows = ws.ws_row;
     return 0;
   }
+}
+
+/*** append buffer ***/
+/*
+since C doesnt have dynamic strings, we need to create our own dynamic string type
+that supports an appending operation
+it consists of a pointer to our buffer in memory and a length
+we define an ABUF_INIT constant which represents an empty buffer, this acts as a constructor to our abuf type
+*/
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
 }
 /*** output ***/
 
